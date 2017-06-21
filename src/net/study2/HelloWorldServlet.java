0dfd55e7cf53341c8be1a9f6e279b6f607bcfbc7
JWOOYONG\jwooyong <jwooyong@gmail.com>
2017-06-21 10:07:42 +0900

    Servlet으로 Hello World 출력하기

---
+++ src/net/study2/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package net.study2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();		
+		out.print("<h1>Hello World Servlet</h1>");
+	}
+}
